750579529c96da497f273b8f61604644e1c0d0ab
sarang <sarang.office01@gmail.com>
2025-12-04 20:07:06 +0530

    new files has been added

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,30 +1,42 @@
-import React, { useContext } from 'react'
-import {assets} from '../assets/assets'
-import { useNavigate } from 'react-router-dom'
-import { AdminContext } from '../context/AdminContext'
-import { DoctorContext } from '../context/DoctorContext'
+import React, { useContext } from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { AdminContext } from "../context/AdminContext";
+import { DoctorContext } from "../context/DoctorContext";
 
 const Navbar = () => {
-  const { dToken, setDToken } = useContext(DoctorContext)
-  const { aToken, setAToken } = useContext(AdminContext)
-    const navigate = useNavigate()
+  const { dToken, setDToken } = useContext(DoctorContext);
+  const { aToken, setAToken } = useContext(AdminContext);
+  const navigate = useNavigate();
 
-    const logout = () => {
-        navigate('/')
-        dToken && setDToken('')
-        dToken && localStorage.removeItem('dToken')
-        aToken && setAToken('')
-        aToken && localStorage.removeItem('aToken')
-      }
+  const logout = () => {
+    navigate("/");
+    dToken && setDToken("");
+    dToken && localStorage.removeItem("dToken");
+    aToken && setAToken("");
+    aToken && localStorage.removeItem("aToken");
+  };
   return (
-    <div className='flex justify-between items-center px-4 sm:px-12 py-1 border-b bg-white'>
-      <div className='flex items-center gap-2 text-xs'>
-        <img onClick={() => navigate('/')} className='w-28 sm:w-32 cursor-pointer' src={assets.logo1} alt="" />
-        <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
+    <div className="flex justify-between items-center px-4 sm:px-12 py-1 border-b bg-white">
+      <div className="flex items-center gap-2 text-xs">
+        <img
+          onClick={() => navigate("/")}
+          className="w-28 sm:w-32 cursor-pointer"
+          src={assets.logo1}
+          alt=""
+        />
+        <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">
+          {aToken ? "Admin" : "Doctor"}
+        </p>
       </div>
-      <button onClick={logout} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
+      <button
+        onClick={logout}
+        className="bg-primary text-white text-sm px-10 py-2 rounded-full"
+      >
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
