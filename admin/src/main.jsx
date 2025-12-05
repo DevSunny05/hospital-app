9a232dad7b71ce4a0717e4aa75b47269e67b4b32
sarang <sarang.office01@gmail.com>
2025-12-05 21:19:11 +0530

    new files has been added

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,22 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import AdminContextProvider from './context/AdminContext.jsx'
-import DoctorContextProvider from './context/DoctorContext.jsx'
-import AppContextProvider from './context/AppContext.jsx'
-import axios from 'axios'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AdminContextProvider from "./context/AdminContext.jsx";
+import DoctorContextProvider from "./context/DoctorContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
+import axios from "axios";
 
-// Setting the base URL for the backend
-// In production (Vercel): Use VITE_BACKEND_URL environment variable
-// In development: If not set, relative paths will be proxied to localhost:8080 by Vite
-const backendUrl = import.meta.env.VITE_BACKEND_URL
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 if (backendUrl) {
-  axios.defaults.baseURL = backendUrl
+  axios.defaults.baseURL = backendUrl;
 }
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AdminContextProvider>
       <DoctorContextProvider>
@@ -26,5 +23,4 @@ createRoot(document.getElementById('root')).render(
       </DoctorContextProvider>
     </AdminContextProvider>
   </BrowserRouter>
-
-)
+);
